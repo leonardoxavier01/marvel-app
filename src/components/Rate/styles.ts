415a003a459e2be88338a670fcf58cab903ddbe9
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2022-10-21 09:48:29 -0300

    feat: rate component

---
+++ src/components/Rate/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
+  align-items: flex-start;
+  p {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Radio = styled.input`
+  display: none;
+`;
+
+export const Rating = styled.div`
+  cursor: pointer;
+`;
+
+export const WrapperRate = styled.div`
+  display: flex;
+`;
